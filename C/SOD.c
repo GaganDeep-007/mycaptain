3341805971dff781508d50235babe77737c0f4c1
GaganDeep-007 <75296463+GaganDeep-007@users.noreply.github.com>
2021-12-27 20:37:06 +0530

    SOD.c added
    sod is sum of digits

---
+++ C/SOD.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+int sumOfDigits(int num);
+int main() 
+{ 
+    int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+    int sum = sumOfDigits(num);
+    printf("Sum of digits is %d\n",sum);
+    return 0; 
+}
+int sumOfDigits(int num) 
+{
+    if (num == 0)
+       return 0;
+    return (num%10 + sumOfDigits(num/10));
+}
